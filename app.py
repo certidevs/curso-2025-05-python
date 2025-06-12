8ba36701c95e79b9c389f6cb4fdb8dcd334c9bb0
alansastre <alan@certidevs.com>
2025-06-12 16:33:20 +0200

    Agregar OOP a programa app Products

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 # import app_funciones as defs
 
 # Opción 3: importar solo lo necesario
-from app_funciones import mostrar_productos
+# from app_funciones import mostrar_productos
 
 menu = """
 Aplicación CRUD de productos:
@@ -20,33 +20,61 @@ Aplicación CRUD de productos:
 # esto se puede reemplazar por una lista de objetos de la clase Producto
 
 class Product:
-    pass
+    def __init__(self, name: str, price: float, quantity: int, published: bool):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.published = published
 
-product1 = Product()
+# crear objetos Product
+products = [
+    Product('Producto 1', 19.99, 5, True),
+    Product('Producto 2', 29.99, 10, True)
+]
 
-products = ["ordenador MSI modern 15", "ordenador HP pavilion"]
+def mostrar_productos(products):
+    # Opción 1: imprimir todos los productos de golpe:
+    # print(products)
+
+    # Opción 2: imprimir los productos pero de uno en uno
+    # for product in products:
+    #     print(product)
+
+    # Opción 3: imprimir con el índice:
+    if len(products) == 0:
+        print('No hay productos')
+        return
+    
+    for i, product in enumerate(products):
+        print(f'Producto {i}: {product}')
+
 
 def crear_producto():
-    try:
-        new_product = input("Introduce el nombre del nuevo producto")
 
-        # # Opción 1
-        # # validar que el producto esté correcto
-        if new_product.strip(): #verificar que no esté vacío
-            products.append(new_product)
-            print("producto guardado correctamente")
-        else:
-            print("Error, el producto no puede estar vacío")
+    product_name = input("Introduce el nombre del nuevo producto")
+    if not product_name:
+        print("Nombre incorrecto")
+        return
 
-        # opción 2
-        # if len(new_product) == 0: #cuidado, si el producto solo tiene espacios lo da por bueno sí tendrá longitud
-        #     print("Error, el producto no puede estar vacío")
-        #     return
-        
-        # products.append(new_product)
+    product_price = float(input("Introduce el precio en euros del producto sin prefijo. Ejemplo: 15.99"))
+    if product_price < 0:
+        print ("Precio no puede ser negativo")
+        return
+    
+    product_quantity = int(input("Introduce unidades de producto (stock). Ejemplo: 5"))
+    if product_quantity < 0:
+        print ("Cantidad no puede ser negativo")
+        return
+    
+    product_published_input = input('¿El producto está publicado? (y/n):').lower().strip()
+    # product_published = product_published_input == 'y' or product_published_input == 'yes' or product_published_input == 'si'
+    product_published = product_published_input == 'y'
 
-    except ValueError:
-        print("error al guardar nuevo producto")
+    # ya tenemos todos los valores para crear un nuevo Producto
+    new_product = Product(product_name, product_price, product_quantity, product_published)
+    products.append(new_product)
+    print('Producto creado correctamente')
+
 
 def editar_producto():
     mostrar_productos(products)
@@ -95,3 +123,5 @@ while True:
 
 # En una aplicación web (flask, fastapi, django) es el framework quien controla la ejecución
 # del programa y lo mantiene en marcha
+
+## TODO agregar campo id para simular una clave primaria como en base de datos
